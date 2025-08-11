20b34387f97f671ca064d90a6b9ca4482618bc59
cahuerta <cahuerta@gmail.com>
2025-08-11 16:40:28 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: { outDir: 'dist' } // Vercel sirve dist automáticamente
+  build: { outDir: 'dist' }
 })
