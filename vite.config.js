2290b6d0fbb9e82a6c3c9b4f8f7e6b383042bbe8
cahuerta <cahuerta@gmail.com>
2025-08-11 17:11:10 -0400

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-
-// Sin @vitejs/plugin-react. Compila JSX con esbuild (modo automático).
-export default defineConfig({
-  esbuild: { jsx: 'automatic' },
-  build: { outDir: 'dist' }
-})
